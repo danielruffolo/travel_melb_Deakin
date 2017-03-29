6c092367c21e8081c8963798949d4a8f79ca3000
druffolo <ruffod17@gmail.com>
2017-03-29 14:07:44 +1100

    Initial Commit

---
+++ travel_melb_Deakin/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  travel_melb_Deakin
+//
+//  Created by Daniel Ruffolo on 29/3/17.
+//  Copyright © 2017 Daniel Ruffolo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
